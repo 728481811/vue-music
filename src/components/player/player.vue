4ad61448ba50db598fbaa08d6b295693ddb47004
728481811 <728481811@qq.com>
2017-07-07 12:56:19 +0800

    Update player.vue

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="player">
         <div class="normal-player">
-          12:50
+          12:55
         </div>
         <div class="mini-player">
-          12:53
+          12:55
         </div>
     </div>
 </template>
