e9003188197b3e450e9afb17f521568bb34ad817
728481811 <728481811@qq.com>
2017-07-07 14:54:18 +0800

    Update player.vue

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -2,9 +2,6 @@
     <div class="player">
         <div class="normal-player">
         </div>
-        <div class="mini-player">
-          14:44
-        </div>
     </div>
 </template>
 <script type="text/ecmascript-6">
