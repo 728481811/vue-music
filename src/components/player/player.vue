7b916485477bf08bca61e301bac667d8bfce5a5a
tao <728481811@qq.com>
2017-07-07 10:30:41 +0800

    10:30

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,9 +1,7 @@
 <template>
     <div class="player">
         <div class="normal-player">
-        播放器
         </div>
-        <div class="mini-player"></div>
     </div>
 </template>
 <script type="text/ecmascript-6">
